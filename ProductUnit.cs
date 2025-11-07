eaa7e5b4d1a3c0a63a5659fef1337250a297f332
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-11-07 16:04:12 -0500

    🟥 Cuando_ComproUnKiloDeManzanaAPrecioNormal_ElPrecioTotalDelRecibo_Debe_SerDe_1_99

--- ProductUnit.cs
+++ ProductUnit.cs
@@ -2,5 +2,6 @@
 
 public enum ProductUnit
 {
-    Unit
+    Unit,
+    Kilo
 }
--- SupermarketReceiptTest.cs
+++ SupermarketReceiptTest.cs
@@ -17,5 +17,18 @@ public class SupermarketReceiptTest
         
         receipt.GetTotalPrice().Should().Be(0.99m);
     }
-    
+
+    [Fact]
+    public void Cuando_ComproUnKiloDeManzanaAPrecioNormal_ElPrecioTotalDelRecibo_Debe_SerDe_1_99()
+    {
+        var catalog = new Catalog();
+        var apples = new Product("Apple", 1.99m, ProductUnit.Kilo);
+        catalog.AddProduct(apples);
+        var supermarket = new Supermarket(catalog);
+        
+        supermarket.AddToCart(apples, 1);
+        var receipt = supermarket.Checkout();
+        
+        receipt.GetTotalPrice().Should().Be(1.99m);
+    }
 }
