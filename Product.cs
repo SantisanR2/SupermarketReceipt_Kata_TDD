c953e8a56a98dc3d85d20e23fdef88e2cddb7468
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-11-08 09:02:11 -0500

    🟩 Cuando_ComproSeisTubosDePastaDeDientes_UnKiloDeManzanas_DosCepillosDeDientes_UnaBolsaDeArroz_Y_TresCajasDeTomatesCherryConDescuento_ElPrecioTotalDelRecibo_Debe_SerDe_15_783

--- Product.cs
+++ Product.cs
@@ -3,13 +3,20 @@
 public class Product
 {
     private decimal _price;
+    private string _name;
     public Product(string name, decimal price, ProductUnit unit)
     {
+        _name = name;
         _price = price;
     }
 
     public decimal GetPrice()
     {
         return _price;
+    }
+
+    public string GetName()
+    {
+        return _name;
     }
 }
--- Supermarket.cs
+++ Supermarket.cs
@@ -17,23 +17,23 @@ public class Supermarket
 
         foreach (var product in _productsInCart)
         {
-            if (_discounts.Contains("toothbrush"))
+            if (_discounts.Contains("toothbrush") && product.Key.GetName().Equals("Toothbrush"))
             {
                 products.Add(product.Key, Math.Truncate(product.Value/2m) * product.Key.GetPrice() + (product.Value % 2) * product.Key.GetPrice());
             }
-            else if (_discounts.Contains("apple"))
+            else if (_discounts.Contains("apple") && product.Key.GetName().Equals("Apple"))
             {
                 products.Add(product.Key, product.Value * product.Key.GetPrice() * 0.8m);
             }
-            else if (_discounts.Contains("rice"))
+            else if (_discounts.Contains("rice") && product.Key.GetName().Equals("Rice"))
             {
                 products.Add(product.Key, product.Value * product.Key.GetPrice() * 0.9m);
             }
-            else if (_discounts.Contains("toothpaste"))
+            else if (_discounts.Contains("toothpaste") && product.Key.GetName().Equals("Toothpaste"))
             {
                 products.Add(product.Key, Math.Truncate(product.Value/5m) * 7.49m + (product.Value % 5) * product.Key.GetPrice());
             }
-            else if (_discounts.Contains("tomatoes"))
+            else if (_discounts.Contains("tomatoes") && product.Key.GetName().Equals("Cherry tomatoes"))
             {
                 products.Add(product.Key, Math.Truncate(product.Value/2m) * 0.99m + (product.Value % 2) * product.Key.GetPrice());
             }
