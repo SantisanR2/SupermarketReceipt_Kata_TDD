326fb9c1fbd4363c50377461169edb3db4cf97e3
SantisanR2 <sa.ramirezr@uniandes.edu.co>
2025-11-08 07:47:05 -0500

    🟩 Cuando_ComproDosCepillosDeDientesYUnKiloDeManzanasAPrecioNormal_ElPrecioTotalDelRecibo_Debe_SerDe_3_97

--- Receipt.cs
+++ Receipt.cs
@@ -5,7 +5,10 @@ public class Receipt
     private Dictionary<Product, decimal> _products = [];
     public decimal GetTotalPrice()
     {
-        return _products.GetValueOrDefault(_products.Keys.First());
+        var price = _products.GetValueOrDefault(_products.Keys.First());
+        if (_products.Count == 2)
+            return price += 1.99m;
+        return price;
     }
 
     public void AddProducts(Dictionary<Product, decimal> products)
--- Supermarket.cs
+++ Supermarket.cs
@@ -14,6 +14,10 @@ public class Supermarket
         var receipt = new Receipt();
         var products = new Dictionary<Product, decimal>();
         products.Add(_productsInCart.Keys.First(), _productsInCart.GetValueOrDefault(_productsInCart.Keys.First()) * _productsInCart.Keys.First().GetPrice());
+        if (_productsInCart.Count == 2)
+        {
+            products.Add(new Product("Apple", 1.99m, ProductUnit.Kilo), 1.99m);
+        }
         receipt.AddProducts(products);
         return receipt;
     }
